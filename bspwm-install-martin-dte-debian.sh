7e99a96d8455ab8cd0fbd62720d3cb2052ad63c5
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-02 18:45:50 +0100

    icons and other small fixes

--- bspwm-install-martin-dte-debian.sh
+++ bspwm-install-martin-dte-debian.sh
@@ -53,10 +53,10 @@ echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 sleep 3
 
-sudo apt install -y xorg x11-xserver-utils arandr bspwm sxhkd sddm picom polybar network-manager kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen git lxpolkit lxappearance htop mc imagemagick npm sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm firefox-esr
+sudo apt install -y xorg x11-xserver-utils arandr bspwm sxhkd sddm picom polybar network-manager kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen git lxpolkit lxappearance htop mc imagemagick npm sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm xscreensaver firefox-esr
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ~/google-chrome-stable_current_amd64.deb
+sudo apt install -y ~/bspwm-install-ma-dte/google-chrome-stable_current_amd64.deb
 
 echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 echo " "
@@ -104,47 +104,49 @@ echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 cd ~
 
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     ALL DONE - Rebooting System in 10 seconds"
+#echo "     SUPER + ENTER = Terminal"
+#echo "     SUPER + R = Run menu"
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#sleep 1
+#echo "     Rebooting System in 10 seconds"
+#sleep 1
+#echo "     Rebooting System in 9 seconds"
+#sleep 1
+#echo "     Rebooting System in 8 seconds"
+#sleep 1
+#echo "     Rebooting System in 7 seconds"
+#sleep 1
+#echo "     Rebooting System in 6 seconds"
+#sleep 1
+#echo "     Rebooting System in 5 seconds"
+#sleep 1
+#echo "     Rebooting System in 4 seconds"
+#sleep 1
+#echo "     Rebooting System in 3 seconds"
+#sleep 1
+#echo "     Rebooting System in 2 seconds"
+#sleep 1
+#echo "     Rebooting System in 1 seconds"
+#sleep 1
+#echo "     Rebooting System in 0 seconds"
+#sleep 1
+
 echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 echo " "
-echo "     ALL DONE - Rebooting System in 10 seconds"
+echo "     ALL DONE - Press [Enter] to Rebooting System..."
 echo "     SUPER + ENTER = Terminal"
 echo "     SUPER + R = Run menu"
 echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-sleep 1
-echo "     Rebooting System in 10 seconds"
-sleep 1
-echo "     Rebooting System in 9 seconds"
-sleep 1
-echo "     Rebooting System in 8 seconds"
-sleep 1
-echo "     Rebooting System in 7 seconds"
-sleep 1
-echo "     Rebooting System in 6 seconds"
-sleep 1
-echo "     Rebooting System in 5 seconds"
-sleep 1
-echo "     Rebooting System in 4 seconds"
-sleep 1
-echo "     Rebooting System in 3 seconds"
-sleep 1
-echo "     Rebooting System in 2 seconds"
-sleep 1
-echo "     Rebooting System in 1 seconds"
-sleep 1
-echo "     Rebooting System in 0 seconds"
-sleep 1
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     ALL DONE - Rebooting System..."
-echo "     SUPER + ENTER = Terminal"
-echo "     SUPER + R = Run menu"
-echo " "
-echo "     if you are a virtual machine and can not use SUPER KEY"
+echo "     If you are in a virtual machine and can not use SUPER KEY"
 echo "     press CTRL + C to stop and edit the file"
 echo "     nano .config/sxhkd/sxhkdrc"
 echo " "
 echo "     And then you are ready to reboot, sudo reboot"
 echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-sleep 1
+pause 'Press [Enter] key to continue...'
+
 sudo reboot
