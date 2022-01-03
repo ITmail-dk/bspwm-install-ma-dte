9b79a2401196e473327fffa522f97605af8e1a3a
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-03 15:22:34 +0100

    Edit setup

--- setup-menu.sh
+++ setup-menu.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
-PS3='Choose a menu option :'
-smenu=("Install AMD Driver" "Install AMD Driver + NVIDIA" "Install Intel Driver" "Install Intel + NVIDIA Driver" "VM - Copy New SUPER KEY to ALT" "Reboot" "Exit")
-select smenuc in "${smenu[@]}"; do
-    case $smenuc in
-        "1")
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Martin BSPwm DTE - The last step of the installation"
+TITLE="The last step of the installation"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "Install AMD Driver"
+         2 "Install AMD Driver + NVIDIA"
+         3 "Install Intel Driver"
+         4 "Install Intel + NVIDIA Driver"
+         5 "Virtual Machine - Copy New SUPER KEY to ALT KEY"
+         6 "Reboot")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
             sudo apt install -y amd64-microcode
-	        # optionally call a function or run some code here
             ;;
-        "2")
+        2)
             sudo apt install -y amd64-microcode nvidia-detect nvidia-driver
-	        # optionally call a function or run some code here
             ;;
-        "3")
+        3)
             sudo apt install -y intel-microcode
-	        # optionally call a function or run some code here
             ;;
-        "4")
+        4)
             sudo apt install -y intel-microcode nvidia-detect nvidia-driver
-	        # optionally call a function or run some code here
             ;;
-	    "5")
-	        . ~/bspwm-install-ma-dte/copy-vm-keys.sh
-	        ;;
-	    "6")
-	        sudo reboot
-	        ;;
-	    "7")
-	        exit
-	        ;;
-        *) echo "Invalid option. $REPLY";;
-    esac
-done
+        5)
+            . ~/bspwm-install-ma-dte/copy-vm-keys.sh
+            ;;
+        6)
+            sudo reboot
+            ;;
+esac
