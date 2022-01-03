fea512317cc3d43aa0c92e47a67577bc5b6d2696
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-03 19:01:58 +0100

    Update bspwm-install-martin-dte-debian.sh

--- bspwm-install-martin-dte-debian.sh
+++ bspwm-install-martin-dte-debian.sh
@@ -2,11 +2,11 @@
 
 cd ~/bspwm-install-ma-dte
 
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     Creates folders."
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     Creates folders."
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
 mkdir ~/.config
 mkdir ~/.fonts
 mkdir ~/wallpapers
@@ -22,14 +22,14 @@ sudo sh -c "echo 'deb http://cdn-fastly.deb.debian.org/debian/ testing main cont
 sudo sh -c "echo 'deb-src http://cdn-fastly.deb.debian.org/debian/ testing main contrib non-free' >> /etc/apt/sources.list"
 
 # Not for production version
-read -p "Troubleshooting - Press a key to continue the installation."
+#read -p "Troubleshooting - Press a key to continue the installation."
 
 #Copy files to...
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     Copy files to..."
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     Copy files to..."
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 cp -r ~/bspwm-install-ma-dte/config/* ~/.config/
 cp -r ~/bspwm-install-ma-dte/wallpapers/* ~/wallpapers/
@@ -40,36 +40,36 @@ echo "dirs=$HOME/wallpapers;" >> $HOME/.config/nitrogen/nitrogen.cfg
 
 
 #chmod +x files.
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     chmod +x files."
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     chmod +x files."
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/sxhkd/sxhkdrc
 
 chmod +x ~/.config/polybar/run-polybar.sh
 chmod +x ~/.config/rofi/rofi-beats
 
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     All DONE - User + .config files..."
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     All DONE - User + .config files..."
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 # Not for production version
-read -p "Troubleshooting - Press a key to continue the installation."
+#read -p "Troubleshooting - Press a key to continue the installation."
 
 # Add ROOT Level - Software & Packages.
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     APT Install Software & Packages"
-echo " "
-echo "     Enter your user password, to continue if necessary"
-echo " "
-echo "     continues the software installation in 5 seconds"
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     APT Install Software & Packages"
+#echo " "
+#echo "     Enter your user password, to continue if necessary"
+#echo " "
+#echo "     continues the software installation in 5 seconds"
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 sudo apt update
 sudo apt install -y xorg x11-xserver-utils arandr dialog bspwm sxhkd sddm picom polybar network-manager zsh kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen git lxpolkit lxappearance htop mc imagemagick npm feh sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm xscreensaver firefox-esr
@@ -81,7 +81,7 @@ sudo apt install -y ~/bspwm-install-ma-dte/google-chrome-stable_current_amd64.de
 #sudo apt install -y inkscape gimp rawtherapee darktable okular
 
 # Not for production version
-read -p "Troubleshooting - Press a key to continue the installation."
+#read -p "Troubleshooting - Press a key to continue the installation."
 
 # Alacritty - Building and installing ON / OFF
 #echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
@@ -94,28 +94,30 @@ read -p "Troubleshooting - Press a key to continue the installation."
 #. ~/bspwm-install-ma-dte/install-alacritty-debian.sh
 
 # Not for production version
-read -p "Troubleshooting - Press a key to continue the installation."
+#read -p "Troubleshooting - Press a key to continue the installation."
 
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-echo " "
-echo "     ALL DONE - Press [Enter] to Rebooting System."
-echo "     INFO."
-echo "     SUPER + ENTER = Terminal"
-echo "     SUPER + E = File Manager"
-echo "     SUPER + W = Close Program"
-echo "     SUPER + R = Run menu"
-echo " "
-echo "     If you are in a virtual machine and can not use SUPER KEY"
-echo "     press CTRL + C to stop and run file."
-echo " "
-echo "     . copy-vm-keys.sh"
-echo "     "
-echo "     It will make your ALT KEY on the keyboard become the SUPER KEY "
-echo "     As long as you are running in a virtual machine then use it."
-echo " "
-echo "     And then you are ready to reboot, sudo reboot"
-echo " "
-echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+# OLD Install
+# Not for production version
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     ALL DONE - Press [Enter] to Rebooting System."
+#echo "     INFO."
+#echo "     SUPER + ENTER = Terminal"
+#echo "     SUPER + E = File Manager"
+#echo "     SUPER + W = Close Program"
+#echo "     SUPER + R = Run menu"
+#echo " "
+#echo "     If you are in a virtual machine and can not use SUPER KEY"
+#echo "     press CTRL + C to stop and run file."
+#echo " "
+#echo "     . copy-vm-keys.sh"
+#echo "     "
+#echo "     It will make your ALT KEY on the keyboard become the SUPER KEY "
+#echo "     As long as you are running in a virtual machine then use it."
+#echo " "
+#echo "     And then you are ready to reboot, sudo reboot"
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 
 # VM TEST KEY COPY Shall not be included in the published edition.
@@ -129,8 +131,8 @@ echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 # Not for production version
 
-echo "The last step of the installation"
-read -p "Troubleshooting - Press a key to continue the installation."
+#echo "The last step of the installation"
+#read -p "Troubleshooting - Press a key to continue the installation."
 
 
 . last-step-setup-menu.sh
