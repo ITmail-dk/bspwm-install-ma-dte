eb33bee8c646d07406f54d343da1e70a0dcfa6ec
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-04 19:08:54 +0100

    Update apt-install-all-packages.sh

--- install-scripts/apt-install-all-packages.sh
+++ install-scripts/apt-install-all-packages.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Add ROOT Level - Software & Packages.
-#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
-#echo " "
-#echo "     APT Install Software & Packages"
-#echo " "
-#echo "     Enter your user password, to continue if necessary"
-#echo " "
-#echo "     continues the software installation in 5 seconds"
-#echo " "
-#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     APT Install Software & Packages"
+echo " "
+echo "     Enter your user password, to continue if necessary"
+echo " "
+echo "     continues the software installation in 5 seconds"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 sudo apt update
-sudo apt install -y xorg x11-xserver-utils arandr dialog bspwm sxhkd sddm picom polybar network-manager kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen lxpolkit lxappearance htop mc imagemagick npm feh sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm xscreensaver neofetch firefox-esr
+sudo apt install -y xorg x11-xserver-utils arandr dialog bspwm sxhkd sddm picom polybar network-manager kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen lxpolkit lxappearance htop mc imagemagick npm feh sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm xscreensaver curl neofetch firefox-esr
 
+# Google Chrome - Download + Install
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ~/bspwm-install-ma-dte/google-chrome-stable_current_amd64.deb
 
