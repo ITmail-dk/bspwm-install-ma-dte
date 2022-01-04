684267f735cff1a17cb85be9f8f6d745689c77f2
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-05 00:02:05 +0100

    Update apt-install-all-packages.sh

--- install-scripts/apt-install-all-packages.sh
+++ install-scripts/apt-install-all-packages.sh
@@ -15,16 +15,15 @@ sleep 5
 sudo apt update
 sudo apt install -y xorg x11-xserver-utils arandr dialog bspwm sxhkd sddm picom polybar i3lock-fancy network-manager kitty psmisc ranger rofi thunar thunar-archive-plugin thunar-font-manager font-manager gvfs-backends flameshot neovim nitrogen lxpolkit xbacklight lxappearance htop mc imagemagick npm feh sxiv pulseaudio pulsemixer mpd ncmpcpp notify-osd npm xscreensaver curl neofetch firefox-esr
 
-
-sudo apt install -y dunst tlp
-
+sudo apt install -y dunst tlp cmus qt5ct
+# .profile add export QT_QPA_PLATFORMTHEME="qt5ct"
 
 # Google Chrome - Download + Install
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ~/bspwm-install-ma-dte/google-chrome-stable_current_amd64.deb
 
 # More Software & Packages ON / OFF
-sudo apt install -y zsh git wireguard wireguard-tools libreoffice libreoffice-l10n-da inkscape gimp rawtherapee darktable okular virt-manager
+sudo apt install -y zsh git wireguard wireguard-tools libreoffice libreoffice-gtk3 libreoffice-l10n-da inkscape gimp rawtherapee darktable okular virt-manager
 
 
 # More Themes Extra Packages ON / OFF
