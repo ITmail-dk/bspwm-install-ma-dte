a5892b1907bdb198bdef9187112555d2a68eb4df
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2022-01-05 16:02:20 +0100

    single_monocle xwindow dialog

--- install-scripts/test.sh
+++ install-scripts/test.sh
@@ -8,3 +8,35 @@ echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
 
 
 # Test !!
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "       TEST RUN"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+sleep 1
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "       TEST RUN"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+sleep 1
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "       TEST RUN"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+sleep 1
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "       TEST RUN"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+#. ~/bspwm-install-ma-dte/last-step-setup-menu.sh
