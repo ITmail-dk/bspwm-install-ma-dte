200e438bb5c517066aceacb84aa7582d485d3884
ITmail-dk <post@itmail.dk>
2022-02-14 16:27:27 +0100

    test

--- install-ma-dte-debian.sh
+++ install-ma-dte-debian.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd ~/bspwm-install-ma-dte
+#cd ~/bspwm-install-ma-dte
 
 # Do, mkdir, copy, copy, echo files.
-. ~/bspwm-install-ma-dte/install-scripts/mkdir-cp-git-chmod-echo-files.sh
+. $HOME/bspwm-install-ma-dte/install-scripts/mkdir-cp-git-chmod-echo-files.sh
 
 # Add Software & Packages.
-. ~/bspwm-install-ma-dte/install-scripts/apt-install-core-packages.sh
+. $HOME/bspwm-install-ma-dte/install-scripts/apt-install-core-packages.sh
 
 # Add Pulseaudio to system startup as user.
 systemctl --user enable pulseaudio
 
 # Last Software & Packages.
-. ~/bspwm-install-ma-dte/install-scripts/apt-install-software-last-packages.sh
+. $HOME/bspwm-install-ma-dte/install-scripts/apt-install-software-last-packages.sh
 
 # Last step setup menu installing.
-#. ~/bspwm-install-ma-dte/last-step-setup-menu.sh
+#. $HOME/bspwm-install-ma-dte/last-step-setup-menu.sh
