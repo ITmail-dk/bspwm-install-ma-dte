eeec47481145c34410f9fe574161d878a6dd9474
ITmail-dk <post@itmail.dk>
2022-10-14 10:43:18 +0200

    fix

--- install-scripts/dnf-install-core-packages.sh
+++ install-scripts/dnf-install-core-packages.sh
@@ -19,7 +19,7 @@ echo "Enter your user password, to continue if necessary"
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Core packages to be installed
-sudo dnf install -y xorg-x11-server xorg-x11-xinit arandr autorandr dialog bspwm sxhkd sddm picom polybar xautolock xsecurelock network-manager network-manager-applet git kitty psmisc rofi pcmanfm nitrogen dunst cups system-config-printer xsensors cifs-utils lxpolkit xbacklight lxappearance htop mc xarchiver pulseaudio pulsemixer pulseaudio-utils bluez bluez-tools xfce4-pulseaudio-plugin mpv npm cava curl tmux imagemagick sxiv scrot flameshot firefox numlockx ranger neovim neofetch flatpak timeshift ren krename gparted xfce4-power-manager xfce4-settings bpytop
+sudo dnf install -y sddm arandr autorandr dialog bspwm sxhkd picom polybar xautolock xsecurelock network-manager network-manager-applet git kitty psmisc rofi pcmanfm nitrogen dunst cups system-config-printer xsensors cifs-utils lxpolkit xbacklight lxappearance htop mc xarchiver pulseaudio pulsemixer pulseaudio-utils bluez bluez-tools xfce4-pulseaudio-plugin mpv npm cava curl tmux imagemagick sxiv scrot flameshot firefox numlockx ranger neovim neofetch flatpak timeshift ren krename gparted xfce4-power-manager xfce4-settings bpytop
 
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
