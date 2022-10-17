76d73202b80cd6fb86ef712506a22cd7a69473dc
ITmail-dk <post@itmail.dk>
2022-10-17 11:14:31 +0200

    mod menu

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ OPTIONS=(1 "AMD System"
          2 "AMD System + NVIDIA GPU"
          3 "Intel System"
          4 "Intel System + NVIDIA GPU"
-         5 "Virtual Machine System - SUPER KEY to ALT KEY"
+         5 "Virtual Machine System - NO microcode & GPU Drivers"
          6 "Exit, Do nothing and Reboot")
 
 CHOICE=$(dialog --clear \
@@ -28,19 +28,19 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && systemctl reboot
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && sudo reboot
             ;;
         2)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode nvidia-detect nvidia-driver && systemctl reboot
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode nvidia-detect nvidia-driver && sudo reboot
             ;;
         3)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && systemctl reboot
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && sudo reboot
             ;;
         4)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && systemctl reboot
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && sudo reboot
             ;;
         5)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && . ~/bspwm-install-ma-dte/install-scripts/vm-install-mics.sh && systemctl reboot
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo reboot
             ;;
         6)
             systemctl reboot
