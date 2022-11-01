46301bdf369b2e3ae5055d02f7950eb9ae1003b3
ITmail-dk <post@itmail.dk>
2022-11-01 09:04:22 +0100

    new default wallpaper 21-9

--- install-scripts/apt-install-software-last-packages.sh
+++ install-scripts/apt-install-software-last-packages.sh
@@ -13,7 +13,7 @@ echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
 sleep 3
 
 # Software & Packages
-sudo apt install -y wireguard libreoffice libreoffice-gtk3 libreoffice-l10n-da freerdp2-x11 freerdp2-shadow-x11 
+sudo apt install -y wireguard qimgv libreoffice libreoffice-gtk3 libreoffice-l10n-da freerdp2-x11 freerdp2-shadow-x11 
 
 # Software: thunar thunar-archive-plugin thunar-font-manager filezilla notepadqq gigolo netdiscover librecad inkscape gimp rawtherapee darktable okular scribus obs-studio
 
