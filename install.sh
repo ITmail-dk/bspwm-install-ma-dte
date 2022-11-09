43f6546684bd63c6d9e123861866bfc88342c4c3
ITmail-dk <post@itmail.dk>
2022-11-09 09:55:22 +0100

    INSTALL ALL IN TO ONE

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ whiptail --title "The installation of Martin BSPwm DTE on Debian" --menu "Choose
 	"6)" "Exit, Do nothing" 3>&2 2>&1 1>&3
 )
 
+# RUN 1
 
 #sudo sh -c "echo 'Xcursor.theme: Nordzy-cursors' >> /etc/X11/Xresources/x11-common"
 #sudo sh -c "echo 'Xcursor.size: 22' >> /etc/X11/Xresources/x11-common"
@@ -27,17 +28,214 @@ sudo apt update && sudo apt install -y git
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
 
-# Do, mkdir, copy, copy, echo files.
-. ~/bspwm-install-ma-dte/install-scripts/mkdir-cp-git-chmod-echo-files.sh
+# Do, mkdir, copy, copy, echo files. ############################################################################################################################################
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Creates folders."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+mkdir ~/.config
+mkdir ~/.themes
+mkdir ~/.fonts
+mkdir ~/.icons
+mkdir ~/.local
+mkdir ~/.local/bin
+mkdir ~/wallpapers
+mkdir ~/miscellaneous
+# User folders.
+mkdir ~/Downloads
+mkdir ~/Documents
+mkdir ~/Templates
+mkdir ~/Pictures
+mkdir ~/Desktop
+mkdir ~/Public
+mkdir ~/Videos
+mkdir ~/Music
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+#Copy files to...
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Copy files to..."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+cp -r ~/bspwm-install-ma-dte/config/* ~/.config/
+cp -r ~/bspwm-install-ma-dte/fonts/* ~/.fonts/
+#cp -r ~/bspwm-install-ma-dte/themes/* ~/.themes/
+cp -r ~/bspwm-install-ma-dte/music/* ~/Music/
+cp -r ~/bspwm-install-ma-dte/wallpapers/* ~/wallpapers/
+
+cp ~/bspwm-install-ma-dte/local-files/.Xresources ~/
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+# Echo config to files. YES / NO
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Echo config to files..."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+echo "     Add Alias to bashrc under bash_aliases config."
+touch ~/.bashrc
+echo 'alias ls="ls --color=auto --group-directories-first -v -la"' >> ~/.bashrc
+echo 'alias vi="nvim"' >> ~/.bashrc
+echo 'alias upup="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"' >> ~/.bashrc
+
+# Echo Nitrogen config to files.
+echo "dirs=$HOME/wallpapers/;" >> ~/.config/nitrogen/nitrogen.cfg
+
+touch ~/.config/nitrogen/bg-saved.cfg
+echo "[xin_-1]" >> ~/.config/nitrogen/bg-saved.cfg
+echo "file=$HOME/wallpapers/default-wallpaper.jpg" >> ~/.config/nitrogen/bg-saved.cfg
+echo "mode=5" >> ~/.config/nitrogen/bg-saved.cfg
+echo "bgcolor=#191a1b" >> ~/.config/nitrogen/bg-saved.cfg
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+#chmod +x files.
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     chmod +x files."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+
+chmod +x ~/.config/polybar/run-polybar.sh
+chmod +x ~/.config/rofi/powermenu.sh
+chmod +x ~/.config/rofi/rofi-wifi-menu.sh
+chmod +x ~/.config/rofi/rofi-beats.sh
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+#Unzip files to...
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     Unzip files to..."
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+#tar -xf themes/archive.tar.xz -C ~/.themes/
+#tar -xf themes/Nordic-bluish-accent.tar.xz -C ~/.themes/
+#tar -xf themes/cursors/Nordzy-cursors.tar.gz -C ~/.icons/
+
+#Git Clone files.
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Git Clone - Downloads"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+#git clone https://github.com/LINK Nordic-Theme Specific-folder
+
+git clone https://github.com/EliverLara/Nordic ~/bspwm-install-ma-dte/themes/themes-de/Nordic-theme
+git clone https://github.com/alvatip/Nordzy-cursors ~/bspwm-install-ma-dte/themes/cursors/Nordzy-cursors-theme
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme ~/bspwm-install-ma-dte/themes/icons/Papirus-icon-theme
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+#Copy Theme files...
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Copy Theme files..."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+#cp -r ~/bspwm-install-ma-dte/themes/themes-de/* ~/.themes/
+#cp -r ~/bspwm-install-ma-dte/themes/cursors/* ~/.icons/
+#cp -r ~/bspwm-install-ma-dte/themes/icons/* ~/.icons/
+
+cp -r ~/bspwm-install-ma-dte/themes/themes-de/* ~/.themes/
+
+mkdir ~/.icons/Nordzy-cursors
+cp -r ~/bspwm-install-ma-dte/themes/cursors/Nordzy-cursors-theme/Nordzy-cursors/* ~/.icons/Nordzy-cursors
+
+mkdir ~/.icons/Papirus
+mkdir ~/.icons/Papirus-Dark
+mkdir ~/.icons/Papirus-Light
+cp -r ~/bspwm-install-ma-dte/themes/icons/Papirus-icon-theme/Papirus/* ~/.icons/Papirus
+cp -r ~/bspwm-install-ma-dte/themes/icons/Papirus-icon-theme/Papirus-Dark/* ~/.icons/Papirus-Dark
+cp -r ~/bspwm-install-ma-dte/themes/icons/Papirus-icon-theme/Papirus-Light/* ~/.icons/Papirus-Light
+
+mkdir ~/.config/gtk-3.0
+cp ~/bspwm-install-ma-dte/themes/gtk-3.0/settings.ini ~/.config/gtk-3.0
+cp ~/bspwm-install-ma-dte/themes/.gtkrc-2.0 ~/
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
+
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "          All DONE - User + .config files..."
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+clear
 
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
 
 # Add Software & Packages.
-. ~/bspwm-install-ma-dte/install-scripts/apt-install-core-packages.sh
+# Add ROOT Level - Software & Packages.
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     APT Install Software & Packages"
+echo " "
+echo "     Enter your user password, to continue if necessary"
+echo " "
+echo "     continues the software installation in 3 seconds"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+sleep 2
+
+echo "Enter your user password, to continue if necessary"
+
+# APT Update Start RUN
+sudo apt update
+
+# Core packages to be installed ############################################################################################################################################
+sudo apt install -y xorg arandr autorandr dialog bspwm sxhkd sddm picom polybar xautolock xsecurelock network-manager git kitty psmisc rofi nemo thunar pcmanfm module-assistant font-manager gvfs-backends nitrogen dunst rsync cups xsensors smbclient lxpolkit xbacklight lxappearance htop mc xarchiver pulseaudio pulsemixer bluez bluez-firmware bluez-tools bluez-alsa-utils blueman xfce4-pulseaudio-plugin moc mpv curl tmux imagemagick sxiv scrot flameshot firefox-esr numlockx ranger neovim notify-osd neofetch flatpak timeshift rename gparted cpufrequtils xfce4-power-manager xfce4-settings bpytop firmware-linux-nonfree nfs-common virt-viewer gvncviewer
+
+# APT OFF
+# system-config-printer cifs-utils 
 
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
+
+# Google Chrome - Download + Install  ON / OFF
+#rm google-chrome-stable_current_amd64.deb
+cd /tmp/ && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+
+cd ~/bspwm-install-ma-dte
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+
 
 # Add Pulseaudio to system startup as user.
 systemctl --user enable pulseaudio
@@ -45,8 +243,38 @@ systemctl --user enable pulseaudio
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
 
-# Last Software & Packages.
-. ~/bspwm-install-ma-dte/install-scripts/apt-install-software-last-packages.sh
+# Last Software & Packages. ############################################################################################################################################
+# Install - Software & Packages.
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+echo " "
+echo "     Download, Building & Install Software"
+echo " "
+echo "     Enter your user password, to continue if necessary"
+echo " "
+echo "     continues the installation in 3 seconds"
+echo " "
+echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+sleep 1
+
+# Software & Packages
+sudo dnf install -y wireguard-tools librecad freerdp thunar thunar-archive-plugin filezilla gigolo
+
+# Maybe maybe Software & Packages ON / OFF
+#sudo apt install -y 
+
+# Not for production version
+#read -p "Troubleshooting - Press a key to continue the installation."
+
+# Alacritty - Building and installing ON / OFF
+clear
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+#echo " "
+#echo "     Alacritty - Building and installing"
+#echo " "
+#echo "-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-"
+
+# Alacritty - Building and installing ON / OFF
+#. ~/bspwm-install-ma-dte/install-scripts/install-alacritty-debian.sh
 
 # Not for production version
 #read -p "Troubleshooting - Press a key to continue the installation."
@@ -55,24 +283,27 @@ systemctl --user enable pulseaudio
 # Last step setup menu installing.
 #. ~/bspwm-install-ma-dte/last-step-setup-menu.sh
 
+
+# END RUN ##########################################################################################################################################
+
 case $CHOICESTART in
 	"1)")   
-	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && sudo reboot
+	        sudo apt install -y amd64-microcode
 	;;
 	"2)")   
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode nvidia-detect nvidia-driver && sudo reboot
+            sudo apt install -y amd64-microcode nvidia-detect nvidia-driver
 	;;
 
 	"3)")   
-	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && sudo reboot
+	        sudo apt install -y intel-microcode
         ;;
 
 	"4)")   
-	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && sudo reboot
+	        sudo apt install -y intel-microcode nvidia-detect nvidia-driver
         ;;
 
 	"5)")   
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo reboot
+            sudo reboot
         ;;
 
 	"6)")   
@@ -80,10 +311,8 @@ case $CHOICESTART in
         ;;
 esac
 
-
-
 echo "DONE DOEN"
 
 #done
 
-#sudo reboot
+sudo reboot
