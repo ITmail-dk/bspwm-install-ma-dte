dcc5c657ad2b03d8559b52ef2a607663a1103134
ITmail-dk <post@itmail.dk>
2022-11-09 09:06:44 +0100

    NEW Install test

---
+++ install-old.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# APT Update + install
+sudo apt update && sudo apt install -y dialog git
+# Remember that this menu uses this apt package "dialog"
+
+HEIGHT=35
+WIDTH=80
+CHOICE_HEIGHT=10
+BACKTITLE="The installation of Martin BSPwm DTE on Debian"
+TITLE="Martin DTE - BSPwm installation"
+MENU="Choose you system from the following:"
+
+OPTIONS=(1 "AMD System"
+         2 "AMD System + NVIDIA GPU"
+         3 "Intel System"
+         4 "Intel System + NVIDIA GPU"
+         5 "Virtual Machine System - NO microcode & GPU Drivers"
+         6 "Exit, Do nothing and Reboot")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && sudo reboot
+            ;;
+        2)
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode nvidia-detect nvidia-driver && sudo reboot
+            ;;
+        3)
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && sudo reboot
+            ;;
+        4)
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && sudo reboot
+            ;;
+        5)
+            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo reboot
+            ;;
+        6)
+            systemctl reboot
+            ;;
+esac
--- install.sh
+++ install.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
+# NEW Install
+
+CHOICESTART=$(
+whiptail --title "The installation of Martin BSPwm DTE on Debian" --menu "Choose you system from the following" 16 100 9 \
+	"1)" "AMD System"   \
+	"2)" "AMD System + NVIDIA GPU"  \
+	"3)" "Intel System" \
+	"4)" "Intel System + NVIDIA GPU" \
+	"5)" "Virtual Machine System - NO microcode & GPU Drivers" \
+	"6)" "Exit, Do nothing" 3>&2 2>&1 1>&3
+)
+
+
 # APT Update + install
-sudo apt update && sudo apt install -y dialog git
-# Remember that this menu uses this apt package "dialog"
+sudo apt update && sudo apt install -y git
 
-HEIGHT=35
-WIDTH=80
-CHOICE_HEIGHT=10
-BACKTITLE="The installation of Martin BSPwm DTE on Debian"
-TITLE="Martin DTE - BSPwm installation"
-MENU="Choose you system from the following:"
-
-OPTIONS=(1 "AMD System"
-         2 "AMD System + NVIDIA GPU"
-         3 "Intel System"
-         4 "Intel System + NVIDIA GPU"
-         5 "Virtual Machine System - NO microcode & GPU Drivers"
-         6 "Exit, Do nothing and Reboot")
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && sudo reboot
-            ;;
-        2)
+case $CHOICESTART in
+	"1)")   
+	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode && sudo reboot
+	;;
+	"2)")   
             . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y amd64-microcode nvidia-detect nvidia-driver && sudo reboot
-            ;;
-        3)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && sudo reboot
-            ;;
-        4)
-            . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && sudo reboot
-            ;;
-        5)
+	;;
+
+	"3)")   
+	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode && sudo reboot
+        ;;
+
+	"4)")   
+	        . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo apt install -y intel-microcode nvidia-detect nvidia-driver && sudo reboot
+        ;;
+
+	"5)")   
             . ~/bspwm-install-ma-dte/install-ma-dte-debian.sh && sudo reboot
-            ;;
-        6)
-            systemctl reboot
-            ;;
+        ;;
+
+	"6)")   
+		    exit
+        ;;
 esac
+
+#done
